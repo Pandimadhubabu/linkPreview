37c77f459ca424b5eca928d96cf87ac26853a571
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2019-01-19 00:25:52 +0530

    Update generatePreview.js

--- generatePreview.js
+++ generatePreview.js
@@ -45,7 +45,8 @@ const generatePreview = (url, format, callback) => {
           if (format=="json"){
             callback(JSON.stringify({title:title, description: descr, image:img}))
           }else{
-            const html =
+            const html = <title${title}</title>
+
 `<div class="linkPreviewContainer">
                 <a href="${url}" data-href="${url}" title="${url}" rel="nofollow" target="_blank" class="linkPreviewText">
                     <strong>${title}</strong><br>
